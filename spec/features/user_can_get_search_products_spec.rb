19f4e2fe80c7ed0f9b7a94ee84f0c134002a8a8a
Erinna Chen <erinna.chen@gmail.com>
2016-04-26 10:14:57 -0600

    Add user can search for products feature test

---
+++ spec/features/user_can_get_search_products_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature "User visits the root page", :type => :feature do
+  scenario "see items show page" do
+    visit '/'
+    fill_in "Search", with: "sennheiser"
+    click_on "search"
+
+    expect(current_path).to eq "/search"
+    within("#result-10") do
+      expect(page).to have_content "sku: jjdjfd"
+      expect(page).to have_content "name: "
+      expect(page).to have_content "customer average review:"
+      expect(page).to have_content "short description:"
+      expect(page).to have_content "sale price:"
+      expect(page).to have_content "image:"
+    end
+    expect(page).to_not have_css "#result-16"
+  end
+end
